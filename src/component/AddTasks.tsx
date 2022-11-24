f0f121b9a1782932a9fad6b77f934e1792a5c3d7
Pavel Sasnouski <sosnovskiy.paul@gmail.com>
2022-11-24 14:36:22 +0300

    finish

--- src/component/AddTasks.tsx
+++ src/component/AddTasks.tsx
@@ -1,19 +1,11 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable prettier/prettier */
-/* eslint-disable padding-line-between-statements */
 /* eslint-disable no-unused-expressions */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable no-console */
-/* eslint-disable react/button-has-type */
-/* eslint-disable import/no-useless-path-segments */
-/* eslint-disable import/order */
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { dataBase, getStorageFirebase } from './../firebase';
+
+import { dataBase, getStorageFirebase } from '../firebase';
 
 export const initialState = {
   title: '',
@@ -23,6 +15,11 @@ export const initialState = {
 };
 export type InitialStateType = typeof initialState;
 
+/**
+ * Components add new task
+ * @component
+ * Добавляем заголовок, описание, фотографию, дату окончания задачи
+ */
 export const AddTasks: React.FC = () => {
   const [data, setData] = useState<InitialStateType>(initialState);
   // destructuring data
@@ -34,6 +31,11 @@ export const AddTasks: React.FC = () => {
     files && uploadFiles();
   }, [files]);
 
+  /**
+   * Function submit
+   * @param e FormEvent используется для событий onSubmit
+   * функция асинхронная, для размещения в storege
+   */
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await addDoc(collection(dataBase, 'ToDoList'), {
@@ -42,14 +44,24 @@ export const AddTasks: React.FC = () => {
       deadline: deadlineTime,
     });
     setData(initialState);
+    setFiles(null);
   };
+
+  /**
+   * Function для ввода событий в формы
+   * @param e ChangeEvent<HTMLInputElement>
+   */
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
+  /**
+   * Function для загрузки файлов в Storege
+   */
   const uploadFiles = () => {
     const storegeRef = ref(getStorageFirebase, files.name);
     const uploadTask = uploadBytesResumable(storegeRef, files);
+
     uploadTask.on(
       'state_changed',
       null,
@@ -83,21 +95,27 @@ export const AddTasks: React.FC = () => {
           className="task_input"
         />
         <div>
-          <label className="custom-file-upload">
-            Custom Upload
-            <input type="file" onChange={(e: any) => setFiles(e.target.files[0])} />
+          <label htmlFor="file" className="file-upload">
+            Загрузить{' '}
+            <input
+              type="file"
+              id="file"
+              onChange={(e: any) => setFiles(e.target.files[0])}
+            />
+            <div className={files ? `upload` : `upload-no`}>✓</div>
           </label>
-          <label className="custom-date_label">
+          <label htmlFor="date" className="date_label">
             Дата окончания
             <input
               type="date"
+              id="date"
               onChange={(e: any) => setDeadlineTime(e.target.value)}
-              className="custom-date"
+              className="date"
               required
             />
           </label>
         </div>
-        <button>Add</button>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
--- src/component/TasksList.tsx
+++ src/component/TasksList.tsx
@@ -1,12 +1,5 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-console */
-/* eslint-disable prettier/prettier */
-/* eslint-disable padding-line-between-statements */
-/* eslint-disable object-shorthand */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable no-magic-numbers */
 /* eslint-disable prefer-const */
+
 import React, { ChangeEvent, useState, useEffect } from 'react';
 
 import dayjs from 'dayjs';
@@ -23,7 +16,11 @@ type firebaseStateType = {
   deadline: any;
   title: string;
 };
-
+/**
+ * Components output tasks
+ * @component
+ * Вывод списка задач
+ */
 export const TasksList: React.FC = () => {
   const [tasks, setTasks] = useState<firebaseStateType[]>([]);
   const [loading, setLoading] = useState(false);
@@ -48,6 +45,11 @@ export const TasksList: React.FC = () => {
     });
   }, []);
 
+  /**
+   * Fucntion для ввода событий в формы
+   * @param id идентифокатор задачи
+   * @returns handleChange функцию обработчик
+   */
   const handleChangeID = (id: string) => {
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
       setTasks(
@@ -56,9 +58,17 @@ export const TasksList: React.FC = () => {
         ),
       );
     };
+
     return handleChange;
   };
 
+  /**
+   * Fucntion асинхронная для изменения загаловка или описания задачи
+   * @param task  задача  ввиде объекта firebaseStateType
+   * @param title  загаловок ввиде строки
+   * @param descr  описание ввиде строки
+   * @param id  индентификационный номер задачи ввиде строки
+   */
   const handleEdit = async (
     task: firebaseStateType,
     title: string,
@@ -66,16 +76,25 @@ export const TasksList: React.FC = () => {
     id: string,
   ) => {
     try {
-      await updateDoc(doc(dataBase, 'ToDoList', task.id), { title: title, descr: descr });
+      await updateDoc(doc(dataBase, 'ToDoList', task.id), { title, descr });
     } catch (error) {
       console.log(error);
     }
   };
 
+  /**
+   * Fucntion асинхронная для отметки выполненой задачи
+   * @param task задача  ввиде объекта firebaseStateType
+   */
+
   const toggleComplete = async (task: firebaseStateType) => {
     await updateDoc(doc(dataBase, 'ToDoList', task.id), { completed: !task.completed });
   };
 
+  /**
+   * Fucntion асинхронная для удаления задачи
+   * @param id индентификационный номер задачи ввиде строки
+   */
   const handleDelete = async (id: string) => {
     try {
       await deleteDoc(doc(dataBase, 'ToDoList', id));
@@ -85,69 +104,96 @@ export const TasksList: React.FC = () => {
     }
   };
 
-  const transforDate = (time: any) => {
+  /**
+   * Fucntion для преобразования времени в требуемый формат
+   * @param time  время  ввиде числа
+   * @returns  дату ввиде строки
+   */
+  const transforDate = (time: number) => {
     return dayjs(time).format('DD.MM.YY');
   };
-  const deadlineDate = (dedline: any, timeCreate: any) => {
-    return dayjs(Date.parse(dedline) - timeCreate).format('DD');
+
+  /**
+   * Fucntion для преобразования времени в требуемый формат
+   * @param dedline дата к которой необходимо закончить задачу ввиде строки
+   * @returns возвращает количество оставшихся дней
+   */
+  const deadlineDate = (dedline: string) => {
+    let deadtime = dayjs(Date.parse(dedline) - Date.now()).format('DD');
+
+    return Date.parse(dedline) <= Date.now() ? '00' : deadtime;
   };
 
   return (
     <div className="list">
       {tasks &&
-        tasks.map((task: firebaseStateType) => (
-          <div className="task" key={task.id}>
-            <input
-              type="text"
-              name="title"
-              value={task.title}
-              onChange={handleChangeID(task.id)}
-              onKeyDown={e => {
-                if (e.keyCode === 13) handleEdit(task, task.title, task.descr, task.id);
-              }}
-              className="task_title"
-            />
-            <input
-              type="text"
-              name="descr"
-              value={task.descr}
-              onChange={handleChangeID(task.id)}
-              onKeyDown={e => {
-                if (e.keyCode === 13) handleEdit(task, task.title, task.descr, task.id);
-              }}
-              className="task_descr"
-            />
-            <img src={task.file} alt="img" className="task_img" />
+        tasks
+          .sort(
+            (first: firebaseStateType, last: firebaseStateType) =>
+              last.timestamp - first.timestamp,
+          )
+          .map((task: firebaseStateType) => (
+            <div className="task" key={task.id}>
+              <input
+                type="text"
+                name="title"
+                value={task.title}
+                onChange={handleChangeID(task.id)}
+                onKeyDown={e => {
+                  if (e.keyCode === 13) handleEdit(task, task.title, task.descr, task.id);
+                }}
+                className={`task_title  ${task.completed ? 'task-complite' : ''}`}
+              />
+              <input
+                type="text"
+                name="descr"
+                value={task.descr}
+                onChange={handleChangeID(task.id)}
+                onKeyDown={e => {
+                  if (e.keyCode === 13) handleEdit(task, task.title, task.descr, task.id);
+                }}
+                className="task_descr"
+              />
+              <img src={task.file} alt="img" className="task_img" />
 
-            <p className="task_time-start">{transforDate(task.timestamp)}</p>
-            <p className="task_time-finish">{transforDate(task.deadline)}</p>
-            <p className="task_time-deadline">
-              deadline {deadlineDate(task.deadline, task.timestamp)}
-            </p>
-
-            <button
-              type="button"
-              onClick={() => toggleComplete(task)}
-              className="task_done"
-            >
-              Done
-            </button>
-            <button
-              type="button"
-              onClick={() => handleEdit(task, task.title, task.descr, task.id)}
-              className="task_edit"
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              onClick={() => handleDelete(task.id)}
-              className="task_del"
-            >
-              Del
-            </button>
-          </div>
-        ))}
+              <p className="task_time task_time-start">
+                от: {transforDate(task.timestamp)}
+              </p>
+              <p
+                className={`task_time task_time-finish ${
+                  Date.parse(task.deadline) <= Date.now() || task.completed
+                    ? 'task_time-end'
+                    : ''
+                }`}
+              >
+                до: {transforDate(task.deadline)}
+              </p>
+              <p className="task_time task_time-deadline">
+                осталось {deadlineDate(task.deadline)} дней
+              </p>
+              <button
+                type="button"
+                onClick={() => toggleComplete(task)}
+                className="task_btn task_done"
+              >
+                Done
+              </button>
+              <button
+                type="button"
+                onClick={() => handleEdit(task, task.title, task.descr, task.id)}
+                className="task_btn task_edit"
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                onClick={() => handleDelete(task.id)}
+                className="task_btn task_del"
+              >
+                Del
+              </button>
+            </div>
+          ))}
     </div>
   );
 };
